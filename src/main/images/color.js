2d15697771880ac141829a2da96d4eba1d256d8f
tangzuodong <tangzuodong@bijiayin.cn>
2023-04-25 18:45:13 +0800

    mod: 对比度

--- src/main/images/color.js
+++ src/main/images/color.js
@@ -15,7 +15,7 @@ export function extract_theme_colors(box) {
   if (iter < 2) iter = 4
   if (iter > 128) iter = 128
 
-  return extract_colors(pixels, iter);
+  return check_color(extract_colors(pixels, iter));
 }
 
 /**
@@ -157,6 +157,61 @@ export function hex_to_color(hex) {
 }
 
 
+
+const sRGBtrc = 2.218;	
+const Rco = 0.2126;		// sRGB Red Coefficient
+const Gco = 0.7156;		// sRGB Green Coefficient
+const Bco = 0.0722;		// sRGB Blue Coefficient
+
+const scaleBoW = 161.8;	// Scaling for dark text on light (phi * 100)
+const scaleWoB = 161.8;	// Scaling for light text on dark â€” same as BoW, but
+
+const normBGExp = 0.38;		// Constants for Power Curve Exponents.
+const normTXTExp = 0.43;	// One pair for normal text,and one for REVERSE
+const revBGExp = 0.5;		// FUTURE: These will eventually be dynamic
+const revTXTExp = 0.43;		// as a function of light adaptation and context
+
+const blkThrs = 0.02;	// Level that triggers the soft black clamp
+const blkClmp = 1.75;	// Exponent for the soft black clamp curve
+
+
+function calc_trc(val) {
+  return (val / 255) ** sRGBtrc;
+}
+
+function calc_sapc(c1, c2) {
+  let sapc = 0;
+  const Rbg = calc_trc(c1[0]);
+  const Gbg = calc_trc(c1[1]);
+  const Bbg = calc_trc(c1[2]);
+
+  const Rtxt = calc_trc(c2[0]);
+  const Gtxt = calc_trc(c2[1]);
+  const Btxt = calc_trc(c2[2]);
+
+  var Ybg = Rbg * Rco + Gbg * Gco + Bbg * Bco
+  var Ytxt = Rtxt * Rco + Gtxt * Gco + Btxt * Bco
+
+  if (Ybg > Ytxt) {
+    // For normal polarity, black text on white
+
+    // soft clamp darkest color if near black.
+    Ytxt = Ytxt > blkThrs ? Ytxt : Ytxt + Math.abs(Ytxt - blkThrs) ** blkClmp
+    sapc = (Ybg ** normBGExp - Ytxt ** normTXTExp) * scaleBoW
+
+    return sapc < 15 ? 0 : sapc.toPrecision(3)
+  } else {
+    // For reverse polarity, white text on black
+    Ybg = Ybg > blkThrs ? Ybg : Ybg + Math.abs(Ybg - blkThrs) ** blkClmp
+    sapc = (Ybg ** revBGExp - Ytxt ** revTXTExp) * scaleWoB
+
+    return sapc > -15 ? 0 : sapc.toPrecision(3)
+  }
+}
+
 export function calc_color_diff(c1, c2) {
-  return Math.pow(c1[0] - c2[0], 2) + Math.pow(c1[1] - c2[1], 2) + Math.pow(c1[2] - c2[2], 2)
+  let sapc = calc_sapc(c1, c2);
+  // console.log(sapc);
+  return Math.pow(sapc, 2);
+  // return Math.pow(c1[0] - c2[0], 2) + Math.pow(c1[1] - c2[1], 2) + Math.pow(c1[2] - c2[2], 2)
 }
